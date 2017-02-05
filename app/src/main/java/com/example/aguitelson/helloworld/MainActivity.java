1b6d5fb6167eb00fabf365833c112a417943e0bf
petrarko <aguitelson@griddynamics.com>
2017-02-05 22:06:04 +0300

    Pictures are god

--- app/src/main/java/com/example/aguitelson/helloworld/MainActivity.java
+++ app/src/main/java/com/example/aguitelson/helloworld/MainActivity.java
@@ -1,34 +1,26 @@
 package com.example.aguitelson.helloworld;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.media.Image;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Environment;
-import android.provider.MediaStore;
-import android.support.annotation.RequiresApi;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static android.R.id.list;
 
 public class MainActivity extends AppCompatActivity {
     private final MainActivity activity = this;
+    static int currentPicture = 0;
+    private static final Random rand = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,17 +28,17 @@ public class MainActivity extends AppCompatActivity {
 
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-        //Remove notification bar
-//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-    //set content view AFTER ABOVE sequence (to avoid crash)
         setContentView(R.layout.activity_main);
         final Button button = (Button) findViewById(R.id.load);
-        button.setOnClickListener(new ButtonListener());
-        loadPicture();
+        button.setOnClickListener(new LoadPictureButtonListener());
+
+        final Button nextButton = (Button) findViewById(R.id.next);
+        nextButton.setOnClickListener(new NextPictureButtonListener());
+        loadPicture(0);
     }
 
-    class ButtonListener implements View.OnClickListener {
+    class LoadPictureButtonListener implements View.OnClickListener {
 
         @Override
         public void onClick(View v) {
@@ -55,30 +47,44 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    class NextPictureButtonListener implements View.OnClickListener {
 
-
-    private void loadPicture(){
-        File storageDir = activity.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File[] list = storageDir.listFiles();
-        for (int i = 0; i < list.length; i++) {
-            if(list[i].getName().endsWith(".jpg")){
-                String photoPath = list[i].getAbsolutePath();
-
-
-
-                BitmapFactory.Options options = new BitmapFactory.Options();
-                options.inPreferredConfig = Bitmap.Config.ARGB_8888;
-                Bitmap bitmap = BitmapFactory.decodeFile(photoPath, options);
-
-                ImageView imageView = (ImageView) findViewById(R.id.imageView);
-                imageView.setImageBitmap(bitmap);
-                return;
-            }
-
+        @Override
+        public void onClick(View v) {
+            loadPicture(rand.nextInt(getListOfFiles().size()));
         }
+    }
+
+
+    private void loadPicture(int index) {
+
+
+        String photoPath = getListOfFiles().get(index).getAbsolutePath();
+
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+        Bitmap bitmap = BitmapFactory.decodeFile(photoPath, options);
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setImageBitmap(bitmap);
+        imageView.setMaxHeight(420);
+        imageView.setMinimumHeight(420);
+
 
     }
 
+    private List<File> getListOfFiles() {
+        File storageDir = activity.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File[] list = storageDir.listFiles();
+        ArrayList<File> files = new ArrayList<>();
+        for (File file : list) {
+            if (file.getName().endsWith(".jpg") && file.length() != 0) {
+                files.add(file);
+            }
+        }
+        return files;
+    }
 
 
 }
